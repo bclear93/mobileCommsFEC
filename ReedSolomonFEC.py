d4f6a1064dcc1d3ccc7113fb6f19c166822f2958
bengyrules <bclear@tcd.ie>
2015-12-17 17:45:15 +0000

    Created functions
    Created functions to match the interfaces

--- ReedSolomonFEC.py
+++ ReedSolomonFEC.py
@@ -1,11 +1,17 @@
 from reedsolo import RSCodec
 rs = RSCodec(10)
+
+def encodeFEC(chrString):
+    val = rs.encode(chrString)
+    return val
+
+def decodeFEC(decodeVal):
+    nval = rs.decode(decodeVal)
+    return nval
+
+
 chrString = '1001100111'
-val = rs.encode(chrString)
+val = encodeFEC(chrString)
 print val
-
-nval = rs.decode(val)
-print nval
-
-
+newVal = decodeFEC(val)
 
